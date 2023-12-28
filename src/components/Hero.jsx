081180ef445a1cdb2b43b27b46bae614fffcf2d6
katarighe <m.ighe@hotmail.com>
2023-12-28 20:38:57 +0300

    Update to include the image background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,21 @@
 import PropTypes from 'prop-types'
 
-function Hero({name, details}) {
+function Hero({ name, details, backgroundImage }) {
+  const sectionStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+  };
+
   return (
-    <section className="bg-green-500 text-white">
-      <div className="container flex flex-col justify-center items-center text-center h-screen gap-0">
+    <section className="bg-green-500 text-orange-400 bg-opacity-100" style={sectionStyle}>
+      <div className="container flex flex-col justify-center items-center text-center h-screen gap-0 bg-opacity-50">
         <h1 className="font-robotoCondensed text-[6rem] p-0 m-0 leading-[80%] uppercase">{name}</h1>
         <h3 className="font-robotoOriginal text-[2rem] p-0 m-0 uppercase">{details}</h3>
       </div>
     </section>
-  )
+  );
 }
 
 Hero.defaultProps = {
