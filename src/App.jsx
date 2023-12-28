3c17a248d6e33eb02e74f4b1b61641da76947fa0
katarighe <m.ighe@hotmail.com>
2023-12-28 19:10:41 +0300

    Add the Homepage and remove the dummy link page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './layouts/Navbar';
-import Hero from './components/Hero';
+import Home from './components/Home';
 
 function App() {
 
@@ -8,9 +8,8 @@ function App() {
     <>
       <Navbar />
       <main >
-        <Hero />
         <Routes>
-          <Route path="/" element={<p>Home</p>} />
+          <Route path="/" element={<Home />} />
           <Route path="/my-projects" element={<p>My Projects</p>} />
           <Route path="/about-me" element={<p>About Me</p>} />
           <Route path="/contact" element={<p>Contact</p>} />
@@ -20,4 +19,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
