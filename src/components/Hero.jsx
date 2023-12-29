4842b26c9e1945f27b7bf41e04f4368c14101dca
katarighe <m.ighe@hotmail.com>
2023-12-29 14:54:17 +0300

    Reverse and fix error

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ function Hero({ name, details, backgroundImage }) {
   };
 
   return (
-    <section className="text-white bg-opacity-100" backgroundImage="src/assets/coding-background.jpg" style={sectionStyle}>
+    <section className="bg-green-500 text-white bg-opacity-100" style={sectionStyle}>
       <div className="container flex flex-col justify-center items-center text-center h-screen gap-0 bg-opacity-50">
         <h1 className="font-bold font-robotoCondensed text-[6rem] p-0 m-0 leading-[80%] uppercase">{name}</h1>
         <h3 className="font-style: italic font-robotoCondensed text-[2.5rem] p-0 m-0 uppercase">{details}</h3>
