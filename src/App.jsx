d310e669056a4c6499c00df7715a6449e87138f4
katarighe <m.ighe@hotmail.com>
2024-01-07 16:39:50 +0300

    Reverse back and remove transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,22 @@
 import { Routes, Route } from "react-router-dom";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
-import { useLocation } from "react-router-dom";
 import Navbar from "./layouts/Navbar";
 import Footer from "./layouts/Footer";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
-import "./styles/page-transition.css";
+import Contact from './pages/Contact';
 
 function App() {
-  const location = useLocation();
-
   return (
     <>
       <Navbar />
       <main>
-        <TransitionGroup>
-          <CSSTransition key={location.key} classNames="page" timeout={500}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/portfolio" element={<Portfolio />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
-          </CSSTransition>
-        </TransitionGroup>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </main>
       <Footer />
     </>
