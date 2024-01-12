1a8476a4f82dc8ff3dcd18c11ccc9941661668fb
katarighe <m.ighe@hotmail.com>
2024-01-12 14:34:33 +0300

    Change color to black

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ function Hero({ name, details, backgroundImage }) {
   };
 
   return (
-    <section className="bg-green-500 text-white bg-opacity-100" style={sectionStyle}>
+    <section className="bg-black text-white bg-opacity-100" style={sectionStyle}>
       <div className="container flex flex-col justify-center items-center text-center h-screen gap-4 bg-opacity-50">
         <h1 className="font-bold font-robotoCondensed text-[5rem] p-0 m-0 leading-[80%] uppercase animate__animated animate__fadeInUp">{name}</h1>
         <h3 className="font-style: italic font-robotoCondensed text-[2rem] p-0 m-0 uppercase tracking-[0.8rem] animate__animated animate__fadeInDown">{details}</h3>
