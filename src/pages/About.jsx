c8d2ec67b86bdbd3ad15b917719154bc7a5cee0c
katarighe <m.ighe@hotmail.com>
2024-01-12 14:54:29 +0300

    Fix the images

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -120,7 +120,7 @@ function AboutMe() {
               Introduction
             </h3>
             <img
-              src="src/assets/background-project.jpg"
+              src="src/assets/mai-profile.JPG"
               alt="About Me"
               className="mr-4 max-w-[20rem] max-h-[20rem] object-right md:float-right md:object rounded-full"
             />
